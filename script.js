866931c02672d4695014e481f8ade4725e92d5e6
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-02-15 08:09:50 -0300

    Add user interface and infinite game loop

---
+++ script.js
@@ -0,0 +1,50 @@
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+
+function getComputerChoice() {
+    let choice = Math.random() * 100;
+
+    if (choice <= 33) {
+        return ROCK;
+    } else if (choice > 66) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (playerChoice === ROCK) {
+        if (computerChoice === PAPER) {
+            return "You lose! Paper beats rock.";
+        } else {
+            return "You win! Rock beats scissors.";
+        }
+    } else if (playerChoice === PAPER) {
+        if (computerChoice === SCISSORS) {
+            return "You lose! Scissors beat paper.";
+        } else {
+            return "You win! Paper beats rock.";
+        }
+    } else if (playerChoice === SCISSORS) {
+        if (computerChoice === ROCK) {
+            return "You lose! Rock beats scissors.";
+        } else {
+            return "You win! Scissors beat paper.";
+        }
+    }
+}
+
+const gameOptions = document.querySelector('#options');
+
+gameOptions.addEventListener('click', (event) => {
+    const playerChoice = event.target;
+
+    const computerChoice = getComputerChoice();
+
+    console.log(playRound(playerChoice.id.toUpperCase(), computerChoice));
+})
