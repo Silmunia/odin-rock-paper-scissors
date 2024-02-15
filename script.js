d38d8a0fa31d307059e36c55d01ec02053d82dff
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-02-15 08:24:28 -0300

    Display player and computer scores in interface

--- script.js
+++ script.js
@@ -20,26 +20,38 @@ function playRound(playerChoice, computerChoice) {
         return "It's a tie!";
     } else if (playerChoice === ROCK) {
         if (computerChoice === PAPER) {
+            computerScore += 1;
             return "You lose! Paper beats rock.";
         } else {
+            playerScore += 1;
             return "You win! Rock beats scissors.";
         }
     } else if (playerChoice === PAPER) {
         if (computerChoice === SCISSORS) {
+            computerScore += 1;
             return "You lose! Scissors beat paper.";
         } else {
+            playerScore += 1;
             return "You win! Paper beats rock.";
         }
     } else if (playerChoice === SCISSORS) {
         if (computerChoice === ROCK) {
+            computerScore += 1;
             return "You lose! Rock beats scissors.";
         } else {
+            playerScore += 1;
             return "You win! Scissors beat paper.";
         }
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 const gameOptions = document.querySelector('#options');
+const resultList = document.querySelector('#results');
+const playerScoreUI = document.querySelector('#player-score');
+const computerScoreUI = document.querySelector('#computer-score');
 
 gameOptions.addEventListener('click', (event) => {
     const playerChoice = event.target;
@@ -48,10 +60,11 @@ gameOptions.addEventListener('click', (event) => {
 
     const result = playRound(playerChoice.id.toUpperCase(), computerChoice);
 
-    const resultList = document.querySelector('#results')
-
     const newListItem = document.createElement('li');
     newListItem.textContent = result;
 
     resultList.appendChild(newListItem);
+
+    playerScoreUI.textContent = "Player Score: " + playerScore;
+    computerScoreUI.textContent = "Computer Score: " + computerScore;
 })
